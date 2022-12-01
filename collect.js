eaad96a8f5be9c547653816dff2af0420d709550
UmarovaZuhra <zukhraumarova17@gmail.com>
2022-12-01 11:54:26 +0500

    tekstlar

---
+++ collect.js
@@ -0,0 +1,61 @@
+// let arr = [
+//     { id: 1, name: "TEMUR", titel: "IT",nuber:6 },
+//     { id: 2, name: "sohib", titel: "muhandis",nuber:4},
+//     { id: 3, name: "karim", titel: "electic",nuber:5 },
+//     { id: 4, name: "karim", titel: "dehqon",nuber:1},
+//     { id: 5, name: "jasur", titel: "bugalter",nuber:2 },
+//     { id: 6, name: "jamshid", titel: "Tuzizm",nuber:8 },
+// ];
+// function DeleteItem(p) {
+//     let newArr = arr.filter((value,index) => value.id !== p);
+//     console.log(newArr);
+// }
+// DeleteItem(3);
+ 
+// search Read
+// let searchItem = function (z) {
+//     let filtered = arr.filter((val) => val.name.includes(z.tolowerCase ())||val.name.includes(z.toUpperCase ())
+//     );
+//     console.log(filtered);
+// };
+// searchItem("k");
+
+//  C create
+
+// let obj = {
+//     func : function (){
+//         console.log(this.name);
+
+//     }
+// };
+
+
+
+
+// let Newproj = {
+//     name: "Zukhra",
+//     title:"dasturchi",
+//     meth:function(){
+//         return "something";
+
+//     },
+// };
+// obj.func.call(Newproj);
+// // obj.func.call({name:"zukhra"})
+
+// function rrr(a,b) {
+//     let ss = a.split("");
+//     let ss7 = b.split("");
+//     let newarr = ss.map ((val)=>Number(val)).filter((value)=>value*0==0).join("");
+//     let newarr1 = ss7.map ((val)=>Number(val)).filter((value)=>value*0==0).join("");
+//     console.log(newarr,newarr1);
+    
+// };
+
+
+
+///////////////////////////////////
+// let val = "Some magic value";
+// console.log(val);
+// val = "yes is magic";
+// console.log(val);
